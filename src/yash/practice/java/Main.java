faa2e1025e03938cdb8903519025b72649f7369a
yash-bhardwaj <yash.bhardwaj1993@gmail.com>
2021-04-28 17:17:08 +0530

    Triangle patterns

--- src/yash/practice/java/Main.java
+++ src/yash/practice/java/Main.java
@@ -4,20 +4,44 @@ import yash.practice.java.custom.singlylinkedlist.Node;
 
 public class Main {
     public static void main(String[] args) {
-        Node<Integer> list = new Node<>();
-        Node<Integer> head = null;
+//        Node<Integer> list = new Node<>();
+//        Node<Integer> head = null;
+//
+//
+//        head = list.add(head, 10);
+//        head = list.add(head, 100);
+//        head = list.add(head, 104);
+//        head = list.add(head, 102);
+//        head = list.add(head, 100);
+//        head = list.addNodeOnIndexFromEnd(head, 111, 5);
+//        list.printList(head);
+//        System.out.println();
+//        head = head.reverse(head);
+//        list.printList(head);
 
+        for(int i=0; i<10;i++) {
+            for(int j=10; j>i-1;j--) {
+                System.out.print("");
+            }
+            for(int j=i; j>=0;j--) {
+                System.out.print("* ");
+            }
 
-        head = list.add(head, 10);
-        head = list.add(head, 100);
-        head = list.add(head, 104);
-        head = list.add(head, 102);
-        head = list.add(head, 100);
-        head = list.addNodeOnIndexFromEnd(head, 111, 5);
-        list.printList(head);
+//            for(int j=10; j>=i+1;j--) {
+//                System.out.print(" *");
+//            }
+            System.out.println();
+        }
         System.out.println();
-        head = head.reverse(head);
-        list.printList(head);
+        for(int i=0; i<10;i++) {
+            for(int j=0; j<=i;j++) {
+                System.out.print("  ");
+            }
+            for(int j=10; j>=i+1;j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
     }
 }
